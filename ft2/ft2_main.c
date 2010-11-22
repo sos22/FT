14ea87b8417adc9aab4b7e361a04b1450da55dc1
Steven Smith <sos22@cl.cam.ac.uk>
2010-11-22 11:04:19 +0000

    Add a new tool, ft2, for a different approach to the alias resolution problem.

---
+++ ft2/ft2_main.c
@@ -0,0 +1,37 @@
+#include "pub_tool_basics.h"
+#include "pub_tool_tooliface.h"
+
+static void
+ft2_post_clo_init(void)
+{
+}
+
+static IRSB *
+ft2_instrument(VgCallbackClosure* closure,
+	       IRSB* bb,
+	       VexGuestLayout* layout,
+	       VexGuestExtents* vge,
+	       IRType gWordTy,
+	       IRType hWordTy)
+{
+	return bb;
+}
+
+static void
+ft2_fini(Int exitcode)
+{
+}
+
+static void
+ft2_pre_clo_init(void)
+{
+	VG_(details_name)("FT2");
+	VG_(details_version)(NULL);
+	VG_(details_description)("foo");
+	VG_(details_copyright_author)("me");
+	VG_(details_bug_reports_to)(VG_BUGS_TO);
+
+	VG_(basic_tool_funcs)(ft2_post_clo_init, ft2_instrument, ft2_fini);
+}
+
+VG_DETERMINE_INTERFACE_VERSION(ft2_pre_clo_init)
